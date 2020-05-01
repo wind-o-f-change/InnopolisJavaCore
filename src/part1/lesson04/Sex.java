646fe6da67ed3edf0dd6f2438dc1f43e129c139d
Anton31 <antohaspy070@gmail.com>
2020-05-01 17:46:49 +0300

    Наброски ДЗ №5

---
+++ src/part1/lesson04/Sex.java
@@ -0,0 +1,16 @@
+package part1.lesson04;
+
+/**
+ * Create 01.05.2020
+ *
+ * @autor Evtushenko Anton
+ */
+
+public enum Sex {
+    MAN("MAN"),
+    WOMAN("WOMAN");
+    private String sex;
+    Sex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/part1/lesson04/run/RunProgram.java
@@ -0,0 +1,20 @@
+package part1.lesson04.run;
+
+import part1.lesson04.Person;
+import part1.lesson04.PetsStore;
+import part1.lesson04.Sex;
+
+/**
+ * Create 01.05.2020
+ *
+ * @autor Evtushenko Anton
+ */
+
+public class RunProgram {
+    public static void main(String[] args) {
+        System.out.println(new Person("Fedor", Sex.MAN, 12));
+
+
+    }
+    private RunProgram(){}
+}
