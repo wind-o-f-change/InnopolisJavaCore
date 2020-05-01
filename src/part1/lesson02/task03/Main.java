433625648bc6845e4afbc15a53ddff238acfe1a0
Anton31 <antohaspy070@gmail.com>
2020-05-01 18:03:02 +0300

    Рефакторинг sortByComparator method

--- src/part1/lesson02/task03/Main.java
+++ src/part1/lesson02/task03/Main.java
@@ -14,6 +14,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        // Стало работать примерно в 2 раза быстрее :)
         PersonStore store = new PersonStore(Person.getPersonArray(10001));
         runTask(store, new SortedByName());
         runTask(store.getPersons(), new SortedByName());
