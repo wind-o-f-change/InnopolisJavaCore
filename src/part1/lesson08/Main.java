0187705d18839b0cc190850f42f657cd40e1eff4
Anton31 <antohaspy070@gmail.com>
2020-05-21 17:33:40 +0300

    исправление ДЗ_5

--- src/part1/lesson08/Main.java
+++ src/part1/lesson08/Main.java
@@ -1,9 +1,7 @@
 package part1.lesson08;
 
 import java.io.*;
-
-import static part1.lesson08.Car.deSerialize;
-import static part1.lesson08.Car.serialize;
+import java.lang.reflect.Field;
 
 /**
  * Create 18.05.2020
@@ -26,5 +24,45 @@ public class Main {
         System.out.println("It's a 2_" + car2);
         car2.beep();
         car2.wheel.print();
+    }
+
+    static void serialize(Object object, String file) {
+        Object o = null;
+        try {
+            o = object.getClass().newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        Field[] fields = o.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+
+            try {
+                field.set(o, field.get(object));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(file))) {
+            ous.writeObject(o);
+        } catch (IOException e) {
+            System.out.println("Упс ...serialize... /-_-/");
+            e.printStackTrace();
+        }
+    }
+
+    static Object deSerialize(String file) {
+        Object obj = null;
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+             obj = ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(String.format("Упс ...deSerialize... %s /-_-/", e));
+            e.printStackTrace();
+        }
+        return obj;
     }
 }
