51c74cfb0adff643994f7cb9d6f29cb409db2d26
Anton31 <antohaspy070@gmail.com>
2020-05-30 23:57:29 +0300

    дз_9

--- src/part1/lesson09/RuntimeCompile.java
+++ src/part1/lesson09/RuntimeCompile.java
@@ -23,9 +23,9 @@ public class RuntimeCompile {
         source.append("import part1.lesson09.Worker;\n");
         source.append("\n");
         source.append("public class SomeClass implements Worker {\n");
-        source.append("\n");
         source.append("    @Override\n");
         source.append("    public void doWork() {\n");
+
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Введите действия для метода \"doWork()\". Для введите с новой строки \"end\"");
             boolean isCoding = true;
@@ -35,24 +35,20 @@ public class RuntimeCompile {
                 else source.append(code + '\n');
             }
         }
-//        source.append("        System.out.println(\"Hello world\");\n");
+
         source.append("    }\n");
         source.append("}\n");
 
-//        File root = new File("\\Users\\Anton\\IdeaProjects\\InnopolisLessons\\JavaCore\\src");
         File root = new File("D:\\");
         File sourceFile = new File(root, "OOP\\SomeClass.java");
         Files.write(sourceFile.toPath(), source.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
-//        Files.write(sourceFile.toPath(), source.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
 
-// Compile source file.
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         compiler.run(null, null, null, sourceFile.getPath());
 
-// Load and instantiate compiled class.
         URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{root.toURI().toURL()});
-        Class<?> cls = Class.forName("OOP.SomeClass", true, classLoader); // Should print "hello".
-        Object instance = cls.newInstance(); // Should print "world".
-        if (instance instanceof Worker) ((Worker) instance).doWork();
+        Class<?> cls = Class.forName("OOP.SomeClass", true, classLoader);
+        Object instance = cls.newInstance();
+        ((Worker) instance).doWork();
     }
 }
